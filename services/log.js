f8c4b21e75cf671a2b09ea5887cae325138d269b
Tim Park <timfpark@gmail.com>
2016-07-17 20:37:30 -0700

    print console log message on enabling logs

--- services/log.js
+++ services/log.js
@@ -3,7 +3,9 @@
 let winston = require('winston');
 let Logger = require('le_node');
 
+console.log('checking for token');
 if (process.env.LOGENTRIES_TOKEN) {
+    console.log('found token');
     winston.add(winston.transports.Logentries, {
         token: process.env.LOGENTRIES_TOKEN
     });
@@ -19,5 +21,11 @@ module.exports = {
     debug: function(log) { winston.log('debug', log); },
     error: function(log) { winston.log('error', log); },
     info:  function(log) { winston.log('info', log); },
-    warn:  function(log) { winston.log('warn', log); }
+    warn:  function(log) { winston.log('warn', log); },
+
+    stream: {
+        write: function(message, encoding) {
+            winston.log('info', message);
+        }
+    }
 };
