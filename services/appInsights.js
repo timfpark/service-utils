f03209b2953b1b9ce3419ec87405f0d9010c0806
Tim Park <timfpark@gmail.com>
2017-01-11 08:05:50 -0800

    move exception handling to service-utils

--- services/appInsights.js
+++ services/appInsights.js
@@ -1,4 +1,11 @@
 let appInsights = require('applicationinsights');
 appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start();
 
+process.on('uncaughtException', ex => {
+    common.services.log.error('uncaught exception: ' + ex);
+    common.services.appInsights.trackException(ex);
+
+    setTimeout(process.exit, 1000);
+});
+
 module.exports = appInsights.getClient();
